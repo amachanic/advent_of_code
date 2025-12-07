bc890388c634ed8b6d8205a0ceb3bf69b38e261e
Adam Machanic <amachanic@hotmail.com>
2025-12-06 19:26:52 -0500

    more simplification

--- 2025/d05/p2.sql
+++ 2025/d05/p2.sql
@@ -21,37 +21,47 @@ FROM
     FROM
     (
         SELECT
-            group_starts.*,
-            SUM(group_starts.grpstart) OVER (ORDER BY group_starts.val) AS grp
+            row_sums.*,
+            COALESCE
+            (
+                SUM
+                (
+                    CASE
+                        WHEN row_sums.rs = 0 THEN 1
+                        ELSE 0
+                    END
+                ) 
+                    OVER
+                    (
+                        ORDER BY row_sums.val, row_sums.start_end DESC
+                        ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING
+                    ),
+                0
+            ) AS grp
         FROM
         (
             SELECT
-                row_sums.*,
-                CASE WHEN LAG(row_sums.rs, 1, 0) OVER (ORDER BY row_sums.val, row_sums.start_end DESC) = 0 THEN 1 ELSE 0 END AS grpstart
+                y.val,
+                y.start_end,
+                SUM(start_end) OVER (ORDER BY y.val, y.start_end DESC ROWS UNBOUNDED PRECEDING) AS rs
             FROM
             (
                 SELECT
-                    y.val,
-                    y.start_end,
-                    SUM(start_end) OVER (ORDER BY y.val, y.start_end DESC ROWS UNBOUNDED PRECEDING) AS rs
-                FROM
-                (
-                    SELECT
-                        CONVERT(BIGINT, LEFT(s.value, CHARINDEX('-', s.value) - 1)) AS s,
-                        CONVERT(BIGINT, REPLACE(RIGHT(s.value, LEN(s.value) - CHARINDEX('-', s.value)), CHAR(13), '')) AS e
-                    FROM STRING_SPLIT(@n, CHAR(10)) AS s
-                    WHERE
-                        s.value LIKE '%-%'
-                ) AS x
-                CROSS APPLY
-                (
-                    VALUES
-                        (x.s, 1),
-                        (x.e, -1)
-                ) AS y (val, start_end)
-            ) AS row_sums
-        ) AS group_starts
+                    CONVERT(BIGINT, LEFT(s.value, CHARINDEX('-', s.value) - 1)) AS s,
+                    CONVERT(BIGINT, REPLACE(RIGHT(s.value, LEN(s.value) - CHARINDEX('-', s.value)), CHAR(13), '')) AS e
+                FROM STRING_SPLIT(@n, CHAR(10)) AS s
+                WHERE
+                    s.value LIKE '%-%'
+            ) AS x
+            CROSS APPLY
+            (
+                VALUES
+                    (x.s, 1),
+                    (x.e, -1)
+            ) AS y (val, start_end)
+        ) AS row_sums
     ) AS final_groups
     GROUP BY
         final_groups.grp
 ) AS fin;
+
